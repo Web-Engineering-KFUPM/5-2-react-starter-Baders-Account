4e65746a58fbd2afa449fdb6f8abd8137117cfa4
Just-Learning-here <bdr-mtr@outlook.com>
2025-09-30 11:22:41 +0300

    Created the StudentCard component

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,13 @@
+function StudentCard(name, id){
+    return (
+        <div>
+            <h3>Name: Bader Almutairi </h3>
+            <p>ID: 202159390 </p>
+            <p>Department: Computer Science </p>
+        
+        </div>
+    );
+
+}
+
+export default StudentCard;
